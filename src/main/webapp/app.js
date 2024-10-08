ea181845e7046bff992e59f38424a1fc65c8fdbe
fabricionetto <agostinhonetto73@gmail.com>
2024-10-08 16:06:18 -0300

    Segundo commit-Requisição e resposta

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -2,3 +2,16 @@
  * 
  */
 
+var xhr = new XMLHttpRequest();
+
+//abrir conexao
+xhr.open("GET","https://jsonplaceholder.typicode.com/posts/1", true);
+
+//gereciamento de estado
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+        console.log(xhr);
+    }
+};
+
+xhr.send();
