4eec6e7e59027beb5c09e3c432fa5e3e26776e71
fabricionetto <agostinhonetto73@gmail.com>
2024-10-08 19:22:45 -0300

    Quinto commit - organização

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,18 +3,21 @@
  */
 
 var xhr = new XMLHttpRequest();
-var documento;
+let metodo = "GET";
+let url = "https://jsonplaceholder.typicode.com/posts/1";
+let documento;
 
 //abrir conexao
-xhr.open("GET","https://jsonplaceholder.typicode.com/posts/1", true);
+xhr.open(metodo, url, true);
 
-//gereciamento de estado
-xhr.onreadystatechange = () => {
+//enviar conexao
+xhr.send();
+
+//gereciamento de estado e recuperação de dados
+xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
 		documento = xhr.response;
 		objdocumento = JSON.parse(documento);
-        console.log(objdocumento.title);
+        console.log(objdocumento);
     }
 };
-
-xhr.send();
