0bf43f86a685b04950865a15de2102a4863e96b3
fabricionetto <agostinhonetto73@gmail.com>
2024-10-08 17:53:09 -0300

    Terceiro commit-transformando string em json

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,14 +3,17 @@
  */
 
 var xhr = new XMLHttpRequest();
+var documento;
 
 //abrir conexao
 xhr.open("GET","https://jsonplaceholder.typicode.com/posts/1", true);
 
 //gereciamento de estado
 xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4) {
-        console.log(xhr);
+    if (xhr.readyState == 4 && xhr.status == 200) {
+		documento = xhr.response;
+		objdocumento = JSON.parse(documento);
+        console.log(objdocumento);
     }
 };
 
