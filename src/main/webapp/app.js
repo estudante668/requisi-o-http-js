cf93eef179eb8260dcea203a2fe1d5ce4cdfc40c
fabricionetto <agostinhonetto73@gmail.com>
2024-10-08 21:24:31 -0300

    Sexto commit-metodo POST

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,23 +1,57 @@
 /**
- * 
+ *  var xhr = new XMLHttpRequest();
+ let metodo = "GET";
+ let url = "https://jsonplaceholder.typicode.com/posts/1";
+ let documento;
+
+ //abrir conexao
+ xhr.open(metodo, url, true);
+
+ //enviar conexao
+ xhr.send();
+
+ //gereciamento de estado e recuperação de dados
+ xhr.onload = () => {
+     if (xhr.status != 200) {
+ 		alert(`Erro: ${ xhr.status} Not Found`);
+ 	} else {
+ 		documento = xhr.response;
+ 		objdocumento = JSON.parse(documento);
+         console.log(objdocumento);
+     }
+ };
+
+ xhr.onprogress = function(event) {
+   if (event.lengthComputable) {
+     alert(`Received ${event.loaded} of ${event.total} bytes`);
+   } else {
+     alert(`Received ${event.total} bytes`); // no Content-Length
+   }
+ };
+
+ xhr.onerror = ()=>{
+ 	console.log("Erro")
+ }
  */
 
+ //Metodo POST
+
 var xhr = new XMLHttpRequest();
-let metodo = "GET";
-let url = "https://jsonplaceholder.typicode.com/posts/1";
-let documento;
-
-//abrir conexao
-xhr.open(metodo, url, true);
-
-//enviar conexao
-xhr.send();
-
-//gereciamento de estado e recuperação de dados
-xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-		documento = xhr.response;
-		objdocumento = JSON.parse(documento);
-        console.log(objdocumento);
-    }
-};
+let metodo = "POST";
+var url = "https://jsonplaceholder.typicode.com/posts";
+var documento = {
+	"userId": 200,
+	"id": 12,
+	"title": "Noticia exclusiva",
+	"body": "Garotos de programa largam jquery apos descobrir quão facil é viver sem ele",
+ }
+ 
+ xhr.open(metodo, url);
+ xhr.send(documento);
+ xhr.onload = () => {
+    
+  		console.log(xhr);
+  	}
+ 
+  
+  
